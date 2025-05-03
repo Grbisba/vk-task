5e527846c1444daf3a8eeafb1bd3920ef124308b
Grbisba <polickarpow.dany@yandex.ru>
2025-05-03 21:52:42 +0300

    add fx run and go mod

--- client/cmd/client/client.go
+++ client/cmd/client/client.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"go.uber.org/fx"
+
+	"github.com/vk-task/client/internal/config"
+)
+
+func main() {
+	fx.New(buildOptions()).Run()
+}
+
+func buildOptions() fx.Option {
+	return fx.Options(
+		fx.Provide(
+			config.New,
+		),
+		fx.Invoke(),
+	)
+}
